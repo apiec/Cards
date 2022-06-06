acbde0a14c7ed5da347b2dff2439574e92c909d4
apiec <apieczul97@gmail.com>
2022-06-06 23:18:49 +0200

    Remove unnecessary usings from Cards.Core

--- Cards.Core/Game.cs
+++ Cards.Core/Game.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Cards.Core;
+﻿namespace Cards.Core;
 
 public class Game
 {   
@@ -14,9 +8,10 @@ public class Game
     {
         BlackCards = new Deck<BlackCard>(blackCards);
         WhiteCards = new Deck<WhiteCard>(whiteCards);
-        State = new StartOfGameState(this);
         Players = new PlayersManager();
         GameOptions = gameOptions;
+        
+        State = new StartOfGameState(this);
     }
 
     public GameOptions GameOptions { get; }

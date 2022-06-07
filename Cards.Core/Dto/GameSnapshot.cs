237b679fd470c9b5a3350b3ec2363c2356c01f2d
apiec <apieczul97@gmail.com>
2022-06-07 22:07:25 +0200

    Add GameSnaphot and GameStateEnum to Cards.Core.Dto

---
+++ Cards.Core/Dto/GameSnapshot.cs
@@ -0,0 +1,11 @@
+﻿using Cards.Core.Game;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Cards.Core.Dto;
+public record GameSnapshot(
+    BlackCard BlackCard,
+    Player Czar,
+    ImmutableList<Player> Players,
+    GameStateEnum GameState
+);

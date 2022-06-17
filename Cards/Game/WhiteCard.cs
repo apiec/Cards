3f0f8fb56252a2283f3dd600f772f229529c3dcf
apiec <apieczul97@gmail.com>
2022-06-17 20:00:20 +0200

    Remove unnecessary data

--- Cards/Game/WhiteCard.cs
+++
@@ -1,2 +0,0 @@
-﻿namespace Cards.Game;
-public record struct WhiteCard(int Id, string Text);
--- Cards/Program.cs
+++ Cards/Program.cs
@@ -1,6 +1,4 @@
 using Cards.Data;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
